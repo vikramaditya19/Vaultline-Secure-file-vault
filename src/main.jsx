a9b83cbfc07bf60ba1ed4bc091691a45e1b50c6e
mittalsukhansh <sukhanshmittal22@gmail.com>
2026-09-07 10:05:38 +0530

    updated vite.con

--- src/main.jsx
+++ src/main.jsx
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-console.log('VITE_API_BASE_URL =', import.meta.env.VITE_API_BASE_URL)
+console.log('VITE_API_BASE_URL =', import.meta.env.VITE_API_BASE_URL) 
